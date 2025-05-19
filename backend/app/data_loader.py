99423d631176070f096301136d0e4d4df9859936
Simon Wong <swongkc13@gmail.com>
2025-05-19 16:53:27 +0200

    Added backend updates

--- backend/app/data_loader.py
+++ backend/app/data_loader.py
@@ -1,18 +1,17 @@
-# app/data_loader.py
-
-import json
 import os
+import json
 from app.config import Config
 from app.mealdb_client import fetch_meals_from_mealdb
 
 def load_or_fetch_meals():
     if os.path.exists(Config.CACHE_FILE):
+        print("📦 Loading meals from cache...")
         with open(Config.CACHE_FILE, "r") as f:
             return json.load(f)
 
-    # Fetch from MealDB
+    print("🌐 Fetching meals from TheMealDB...")
     meals = fetch_meals_from_mealdb()
-    
+
     with open(Config.CACHE_FILE, "w") as f:
         json.dump(meals, f, indent=2)
 
